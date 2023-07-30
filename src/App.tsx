dc42d29fc855823fba6e37661d78bbd6461191f0
Mo7amed676 <84755046+Mo7amed676@users.noreply.github.com>
2023-07-30 16:35:19 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lern React
-        </a>
-      </header>
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1180"
+      height="880"
+      viewBox="0 0 1180 880"
+      fill="none"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M273.163 -222C50.8087 -222 -8.68394 -93.4533 0.983621 28.1071C10.6512 149.668 225.775 209.767 293.985 338.994C338.605 423.528 303.215 749.785 478.009 818.948C765.805 932.824 1212 852.482 1212 852.482V246.776V-222C1212 -222 495.516 -222 273.163 -222Z"
+        fill="#9C5BF5"
+      />
+    </svg>
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Lern React
+  //       </a>
+  //     </header>
+  //   </div>
   );
 }
 
